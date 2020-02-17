3ea25696b16cd627e834278ae6b53dcc91d8774f
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2020-02-17 16:11:40 -0700

    database set up but getting ssl error

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var sessions = require('express-session');
-// var massive = require('massive');
+var massive = require('massive');
 
-
+require('dotenv').config({ path: __dirname + '/.env'});
 
 
 var app = express()
 
-
+massive(process.env.CONNECTION_STRING,{scripts: __dirname + '/db'}).then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log(`The database is connected`)
+}).catch( e => {
+    console.log(process.env.CONNECTION_STRING)
+    console.error(e);
+});
 
 //BODYPARSER INVOKED
 app.use(bodyParser.json());
